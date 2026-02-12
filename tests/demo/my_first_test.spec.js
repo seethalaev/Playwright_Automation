13c161762cd77139cd3ea76772ae63c6b7c5f17e
seethala e v <sheethalev@gmail.com>
2026-02-12 20:30:10 +0530

    asyn,await used

--- tests/demo/my_first_test.spec.js
+++ tests/demo/my_first_test.spec.js
@@ -1,7 +1,7 @@
 const{test,expect}=require('@playwright/test')
 // const{hello,helloworld}=require('../demo/hello')
 // console.log(hello())
-test("This is my first test",({page}) =>{
-    page.goto("https://google.com")
-    
+test("This is my first test",async({page}) =>{
+   await page.goto("https://google.com")
+
 })
