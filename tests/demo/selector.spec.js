a701b1edb1a9af2b28b2e701a53a7c7f4bd5931e
seethala e v <sheethalev@gmail.com>
2026-02-15 21:46:34 +0530

    using x path

--- tests/demo/selector.spec.js
+++ tests/demo/selector.spec.js
@@ -6,7 +6,10 @@ test('Selector demo',async({page})=>{
     //using any object property
     await page.click('id=user-name')
     await page.locator('id=user-name').fill('Seethal')
-    await page.locator['[id="user-name"]'].fill('Anila')
+    await page.locator('[id="user-name"]').fill('Anila')
     //using css selector
-    await page.locator('#login-button').click
+    await page.locator('#login-button').click()
+    //using xpath
+    await page.locator('xpath=//input[@id="password"]').fill('user')
+    await page.locator('//input[@id="password"]').fill('Ravana')
 })
