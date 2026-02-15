6c12cbdcda9e98a66b3c5b51a234ad9b31d2dab8
seethala e v <sheethalev@gmail.com>
2026-02-15 20:15:29 +0530

    using id

--- tests/demo/selector.spec.js
+++ tests/demo/selector.spec.js
@@ -2,4 +2,9 @@ import{test,expect} from '@playwright/test'
 test('Selector demo',async({page})=>{
     await page.goto('https://www.saucedemo.com/')
     await page.pause()
+
+    //using any object property
+    await page('id=user-name')
+    await page.locator('id=user-name').fill('Seethal')
+    await page.locator['[id="user-name"]'].fill('Anila')
 })
