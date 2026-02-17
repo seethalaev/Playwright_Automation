e1c889a069e3142c9a56a060acf95438a52ed3b5
seethala e v <sheethalev@gmail.com>
2026-02-17 21:53:06 +0530

    simple login

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -2,8 +2,13 @@ import{test,expect} from '@playwright/test'
 test('Demo login1',async({page})=>{
     await page.goto('https://demo.applitools.com/')
     await page.pause()
-   // await page.locator('').fill('Seethala')
-   // await page.locator('').fill('')
+
+    await page.locator('[placeholder="Enter your username"]').fill('Seethala')
+    await page.locator('[placeholder="Enter your password"]').fill('123')
+    await page.locator('[id="log-in"]').click()
+   
 
 }
 )
+ 
+ 
