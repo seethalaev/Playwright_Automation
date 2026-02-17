3b5fef27a75491838ab3263fa6a5ffbdab02aed4
seethala e v <sheethalev@gmail.com>
2026-02-17 21:28:52 +0530

    urlloading,then use page inspector for locating elements

---
+++ tests/demo/login.spec.js
@@ -0,0 +1,9 @@
+import{test,expect} from '@playwright/test'
+test('Demo login1',async({page})=>{
+    await page.goto('https://demo.applitools.com/')
+    await page.pause()
+   // await page.locator('').fill('Seethala')
+   // await page.locator('').fill('')
+
+}
+)
