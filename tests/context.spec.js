df65d4c2aafe7265e63da0e0664a9a3010cefb0c
seethala e v <sheethalev@gmail.com>
2026-02-17 15:21:57 +0530

    successfull

--- tests/context.spec.js
+++ tests/context.spec.js
@@ -15,16 +15,12 @@ test('has title', async ({ page, context }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
 
-    await context.tracing.stop({path: 'test1_trace.zip'})
-
-});
-
-test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-
-    // Click the get started link.
+     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
 
-    // Expects page to have a heading with the name of Installation.
+     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+    await context.tracing.stop({path: 'test1_trace.zip'})
 });
+ 
