a91cf0aada0322aca3cf18fe1cb0153d1558335c
seethala e v <sheethalev@gmail.com>
2026-02-17 15:01:34 +0530

    for capyuring screenshots and snapshots

--- tests/context.spec.js
+++ tests/context.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title', async ({ page ,context}) => {
+
+  await context.tracing .start({snapshots:true,screenshots:true}) 
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
