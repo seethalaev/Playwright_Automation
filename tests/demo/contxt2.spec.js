12f513f977134ba0db9732f9d8b07e8a2b3bf4ff
seethala e v <sheethalev@gmail.com>
2026-02-17 15:59:06 +0530

    created context2.spec.js for before all,after all

---
+++ tests/demo/contxt2.spec.js
@@ -0,0 +1,32 @@
+
+
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+let context;
+test.beforeAll(async({browser}) =>
+{
+     context=await browser.newContext()
+     await context.tracing.start
+        ({
+            snapshots: true,
+            screenshots: true
+        })
+})
+test('has title', async ({ page, context }) => {
+
+    
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+
+     // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+     // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+    await context.tracing.stop({path: 'test1_trace.zip'})
+});
+ 
