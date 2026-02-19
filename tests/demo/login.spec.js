3fc13a54f7d4a4aff4cd13bb373334e4f72066d0
seethala e v <sheethalev@gmail.com>
2026-02-19 23:21:33 +0530

    this is done by opening page inspector and record you can get locator from page inspector

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -13,5 +13,12 @@ test('Demo login1', async ({ page }) => {
 test.only('Demo login 2',async({page}) => {
   await page.goto('https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F') 
   await page.pause()
-  
+  await page.getByRole('textbox', { name: 'Email:' }).click();
+  await page.getByRole('textbox', { name: 'Email:' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Email:' }).fill('admin@yourstore.com');
+  await page.getByRole('textbox', { name: 'Password:' }).click();
+  await page.getByRole('textbox', { name: 'Password:' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Password:' }).fill('admin');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
 });
