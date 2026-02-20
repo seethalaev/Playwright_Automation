cdb34901c6981665a33c4ad85a50039dfcb1f9e3
seethala e v <sheethalev@gmail.com>
2026-02-20 23:59:36 +0530

    checking unique element withb this locator

--- tests/aasertion.spec.js
+++ tests/aasertion.spec.js
@@ -1,8 +1,12 @@
-import test,{pae,expect} from '@playwright/test')
+import test,{page,expect} from '@playwright/test'
 test('Assertion demo',async({page}) =>
    {
-         await page.goto('https://kitchen.applitools.com/')
+         await page.goto('https://practicetestautomation.com/practice-test-login/')
          await page.pause()
+         //Asserion
+         //checkthe element is present
+        await expect(page.locator(' name: Username')).toHaveCount(1)
+
    }
 
 )
