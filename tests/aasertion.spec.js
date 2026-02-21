60cb1685d501eba789383fc12caf6848853dd784
seethala e v <sheethalev@gmail.com>
2026-02-21 16:13:53 +0530

    checking have attribute

--- tests/aasertion.spec.js
+++ tests/aasertion.spec.js
@@ -2,7 +2,7 @@ import test,{page,expect} from '@playwright/test'
 test('Assertion demo',async({page}) =>
    {
           await page.goto('https://kitchen.applitools.com/')
-        await page.pause()
+        
          //Asserion
          //checkthe unique element is present
         await expect(page.locator('text=The Kitchen')).toHaveCount(1)
@@ -18,8 +18,12 @@ test('Assertion demo',async({page}) =>
         await expect.soft(page.locator('text=The Kitchen')).toBeDisabled()
         await expect(page.locator('text=The Kitchen')).toBeEnabled()
         //check text
+        await page.pause()
         await expect (page.locator('text=The Kitchen')).toHaveText('The Kitchen')
-        await expect(page.locator('text=The Kitchen')).not.toHaveText('The Kitchen')
+       // await expect(page.locator('text=The Kitchen')).not.toHaveText('The Kitchen')
+        //check attribute value
+        await expect(page.locator('text=The Kitchen')).toHaveAttribute('class',/.*css-dpmy2a/)
+        await expect (page.locator('text=The Kitchen')).toHaveClass(/.*css-dpmy2a/)
 
    }
 )
