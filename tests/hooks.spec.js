a80a61c760181fe6de29301590f6f5f02eba413b
seethala e v <sheethalev@gmail.com>
2026-02-23 20:02:50 +0530

    hoos programme

---
+++ tests/hooks.spec.js
@@ -0,0 +1,14 @@
+import{test,expect} from '@playwright/test'
+test('login',async({page}) =>
+{
+    await page.goto('https://www.saucedemo.com/')
+    await page.pause()
+
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+
+})
