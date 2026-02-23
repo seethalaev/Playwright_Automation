96ae2aa52dd3c084b78de406f8bda1d1b9c30540
seethala e v <sheethalev@gmail.com>
2026-02-23 14:47:24 +0530

    code for login added

--- tests/Slowmotion_videorecrd.spec.js
+++ tests/Slowmotion_videorecrd.spec.js
@@ -6,10 +6,23 @@ test('Slow motion and video recording demo', async () => {
         headless: false
     })
     //create new incognito browser context
-    const context = await browser.newContext()
+    const context = await browser.newContext({
+        recordVideo: {
+            dir: 'videos/',
+            size: { width: 800, height: 600 }
+        }
+    })
     //create new page inside context
     const page = await context.newPage()
-    await page.goto("")
+    await page.goto("https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F")
+    await page.getByRole('textbox', { name: 'Email:' }).click();
+    await page.getByRole('textbox', { name: 'Email:' }).press('ControlOrMeta+a');
+    await page.getByRole('textbox', { name: 'Email:' }).fill('admin@yourstore.com');
+    await page.getByRole('textbox', { name: 'Password:' }).click();
+    await page.getByRole('textbox', { name: 'Password:' }).press('ControlOrMeta+a');
+    await page.getByRole('textbox', { name: 'Password:' }).fill('admin');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
     //dispopse context when t is not needed
     await context.close()
 
