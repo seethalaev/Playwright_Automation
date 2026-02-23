645ea6b88e24310f9ed342640ed57feaf4b5661a
seethala e v <sheethalev@gmail.com>
2026-02-23 14:28:10 +0530

    create new incognito browser

--- tests/Slowmotion_videorecrd.spec.js
+++ tests/Slowmotion_videorecrd.spec.js
@@ -3,6 +3,8 @@ test('Slow motion and video recording demo',async() =>
 {
     //lauch browser
     const browser=await chromium.lauch()
-    
+    //create new incognito browser context
+    const context=await browser.newContext()
+     
     
 })
