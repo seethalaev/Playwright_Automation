9083eec92d7a22e621004239253ba7c6e79241d6
seethala e v <sheethalev@gmail.com>
2026-02-23 14:38:49 +0530

    added option for slowmotion

--- tests/Slowmotion_videorecrd.spec.js
+++ tests/Slowmotion_videorecrd.spec.js
@@ -1,14 +1,16 @@
-import {test,expect} from '@playwright/test'
-test('Slow motion and video recording demo',async() =>
-{
+import { test, expect } from '@playwright/test'
+test('Slow motion and video recording demo', async () => {
     //lauch browser
-    const browser=await chromium.lauch()
+    const browser = await chromium.lauch({
+        slowMo: 500,
+        headless: false
+    })
     //create new incognito browser context
-    const context=await browser.newContext()
+    const context = await browser.newContext()
     //create new page inside context
-    const page=await context.newPage()
+    const page = await context.newPage()
     await page.goto("")
     //dispopse context when t is not needed
     await context.close()
-    
+
 })
