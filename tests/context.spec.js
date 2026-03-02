3f2237a1f3ca790dd49ede64e99142746cf8ee7f
seethala e v <sheethalev@gmail.com>
2026-03-02 23:23:51 +0530

    updated with check heading

--- tests/context.spec.js
+++ tests/context.spec.js
@@ -18,6 +18,9 @@ test('has title', async ({ page, context }) => {
      // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
 
-   
+     // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+    a
 });
  
