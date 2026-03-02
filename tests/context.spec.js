0882e8ca919e80b158b1c886be91bcc3331cb904
seethala e v <sheethalev@gmail.com>
2026-03-02 23:22:57 +0530

    updated with checking title

--- tests/context.spec.js
+++ tests/context.spec.js
@@ -10,6 +10,11 @@ test('has title', async ({ page, context }) => {
             snapshots: true,
             screenshots: true
         })
-    a
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+
+   
 });
  
