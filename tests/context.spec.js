5081f26ba4ed768d286023accffc7642925c7f97
seethala e v <sheethalev@gmail.com>
2026-03-02 23:22:19 +0530

    updated

--- tests/context.spec.js
+++ tests/context.spec.js
@@ -10,17 +10,6 @@ test('has title', async ({ page, context }) => {
             snapshots: true,
             screenshots: true
         })
-    await page.goto('https://playwright.dev/');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-
-     // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
-
-     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-
-    await context.tracing.stop({path: 'test1_trace.zip'})
+    a
 });
  
