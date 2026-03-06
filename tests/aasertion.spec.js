7afec9b0d9b648787aefb83e40f9216448cfb907
seethala e v <sheethalev@gmail.com>
2026-03-06 20:13:44 +0530

    checking element visible

--- tests/aasertion.spec.js
+++ tests/aasertion.spec.js
@@ -6,4 +6,11 @@ test('Assertion demo',async({page}) =>
          //Asserion
          //checkthe unique element is present
         await expect(page.locator('text=The Kitchen')).toHaveCount(1)
-       
+       if(await page.$('text=The Kitchen'))
+       {
+         await page.locator('text=The Kitchen').click()
+       }
+
+       //check elemnt is hidden or visible
+        await expect (page.locator('text=The Kitchen')).toBeVisible()
+      
