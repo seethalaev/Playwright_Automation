87e876c437acad8ce07aa1095b0d65b637b07adb
seethala e v <sheethalev@gmail.com>
2026-03-07 23:31:53 +0530

    clicking open menu

--- tests/hooks.spec.js
+++ tests/hooks.spec.js
@@ -14,12 +14,18 @@ test.describe('All my tests', () => {
     test('home', async ({ page }) => {
 
         await expect(page.locator('[data-test="item-4-img-link"]')).toBeVisible();
-       
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        await expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toBeVisible();
+        await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+        await page.getByText('Get your testing superhero on').click();
+        await page.locator('[data-test="inventory-item-description"]').nth(2).click();
+        await page.locator('[data-test="item-1-img-link"]').click();
+        await expect(page.locator('[data-test="back-to-products"]')).toBeVisible();
+        await page.locator('[data-test="add-to-cart"]').click();
 
     })
     test('log-out', async ({ page }) => {
         await page.getByRole('button', { name: 'Open Menu' }).click();
-        await expect(page.locator('[data-test="inventory-sidebar-link"]')).toBeVisible();
-        await page.locator('[data-test="logout-sidebar-link"]').click();
+       
     })
 })
