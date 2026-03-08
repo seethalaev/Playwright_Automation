150a9fc764da99cb5c7388ddc91c3571dd431f49
seethala e v <sheethalev@gmail.com>
2026-03-08 23:17:15 +0530

    navigate to url and nnavigate to browser forward

--- tests/demo/Learning.spec.js
+++ tests/demo/Learning.spec.js
@@ -1,3 +1,8 @@
 import { test, expect,chromium} from '@playwright/test'
 test('Navigation commands',async({page})=>{
-  
+    //navigate to url
+    page.goto(url)
+    //Browser back button
+    page.goBack()
+    //Browser forward button
+   
