ac486668e22d5e05a38c0e8e567f1c31b77b9664
seethala e v <sheethalev@gmail.com>
2026-03-08 23:17:53 +0530

    nreload currnt page

--- tests/demo/Learning.spec.js
+++ tests/demo/Learning.spec.js
@@ -5,4 +5,8 @@ test('Navigation commands',async({page})=>{
     //Browser back button
     page.goBack()
     //Browser forward button
+    page.goForward()
+    //Reload current page
+    page.reload()
+    //Wait for navigation to complete
    
